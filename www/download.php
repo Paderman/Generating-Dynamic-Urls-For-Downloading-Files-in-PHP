2c596fe18171c97680fb8d723689ae0a84717aed
PadMan <Paderman@web.de>
2020-09-06 05:58:09 +0200

    Update download.php

--- www/download.php
+++ www/download.php
@@ -56,8 +56,6 @@
  </div>
  </div>
  
- <!--script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="  crossorigin="anonymous"></script-->
- 
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
  
  <script>
